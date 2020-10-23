96c12cdab8a248793e5487307ac2bb077d38c5f3
yang-wenhao <wenhao.yang@nttdata.com>
2020-10-23 15:46:10 +0800

    demo上传

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,175 @@
+<template>
+  <div>
+    <div class="navigation-bar">
+      <el-row class="tac">
+        <el-col :span="12">
+          <h5>默认颜色</h5>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+        <el-col :span="12">
+          <h5>自定义颜色</h5>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      address: '1'
+    }
+  },
+  created () {
+    /* //实例化城市查询类
+    var citysearch = new AMap.CitySearch();
+    //自动获取用户IP，返回当前城市
+    var that = this
+    citysearch.getLocalCity(function(status, result) {
+      if (status === 'complete' && result.info === 'OK') {
+        if (result && result.city && result.bounds) {
+          var cityinfo = result.city;
+          var citybounds = result.bounds;
+          that.address = cityinfo
+          console.info(cityinfo)
+          console.info(citybounds)
+          //地图显示当前城市
+          // map.setBounds(citybounds);
+        }
+      } else {
+        document.getElementById('info').innerHTML = result.info;
+      }
+    }); */
+  },
+  methods: {
+    // 展开菜单
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    // 关闭菜单
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+    /* showCityInfo() {
+      //实例化城市查询类
+      var citysearch = new AMap.CitySearch();
+      //自动获取用户IP，返回当前城市
+      citysearch.getLocalCity(function(status, result) {
+        if (status === 'complete' && result.info === 'OK') {
+          if (result && result.city && result.bounds) {
+            var cityinfo = result.city;
+            var citybounds = result.bounds;
+            this.address = cityinfo
+            console.info(cityinfo)
+            console.info(citybounds)
+            //地图显示当前城市
+            // map.setBounds(citybounds);
+          }
+        } else {
+          document.getElementById('info').innerHTML = result.info;
+        }
+      });
+    } */
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.navigation-bar {
+  width: 200px;
+  height: 2000px;
+  background-color: #42B983;
+}
+</style>
